fd3b0bbf8cbc220b4ba558947246d596115c6074
studyVersion <0314sbadre@e-itaca.es>
2023-01-17 14:09:38 +0100

    first commit react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import {useEffect} from "react";
 import './App.css';
+import searchIcon from './search.svg';
+//5f896d0d
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const API_URL = 'http://www.omdbapi.com?apikey=5f896d0d'; 
+
+const movie1 = {
+    "Title": "Spiderman and Grandma",
+    "Year": "2009",
+    "imdbID": "tt1433184",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
+}
+
+const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+    
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, [])
+    return(
+        <div className="app">
+            <h1>MoviesRepo</h1>
+            <div className="search">
+                <input
+                   placeholder="Search for movives"
+                   value=""
+                   onchange ={ () => {} }
+                />
+                <img
+                    src = {searchIcon} 
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
